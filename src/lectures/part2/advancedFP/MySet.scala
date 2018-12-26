e2e2c0cb5cd564421730843cd4cdde29980508d4
SHEFFmachine <sheff@sheffmachine.com>
2018-12-25 21:43:15 -0500

    We are now looking at creating an infinate set.

--- src/lectures/part2/advancedFP/MySet.scala
+++ src/lectures/part2/advancedFP/MySet.scala
@@ -16,6 +16,17 @@ trait MySet[A] extends (A => Boolean) {
   def flatMap[B](f: A => MySet[B]): MySet[B]
   def filter(predicate: A => Boolean): MySet[A]
   def foreach(f: A => Unit): Unit
+
+  /**
+    * Exercise:
+    * - removing an element
+    * - intersection with another set
+    * - difference with another set
+    */
+  def -(elem: A): MySet[A]
+  def --(anotherSet: MySet[A]): MySet[A] // difference
+  def &(anotherSet: MySet[A]): MySet[A] // intersection
+  def unary_! : MySet[A]
 }
 
 class EmptySet[A] extends MySet[A] {
@@ -34,7 +45,92 @@ class EmptySet[A] extends MySet[A] {
 
   override def foreach(f: A => Unit): Unit = () // this does not do anything, will return unit, which is ()
 
+  override def -(elem: A): MySet[A] = this
+
+  override def --(anotherSet: MySet[A]): MySet[A] = this
+
+  override def &(anotherSet: MySet[A]): MySet[A] = this
+
+  // Say you are given Set(1, 2, 3), return everything but that set
+
+  override def unary_! : MySet[A] = new AllInclusiveSet[A]
 }
+
+class AllInclusiveSet[A] extends MySet[A] {
+  override def contains(element: A): Boolean = true // because it has all the values in theory
+
+  override def +(element: A): MySet[A] = this // same reason as above
+
+  override def ++(anotherSet: MySet[A]): MySet[A] = this // same as above
+
+  /**
+    * Imagine I have val naturals =  allInclusiveSet[Int] = all real numbers
+    * What happens when I say naturals map _ % 3 => ???
+    * [0, 1, 2] from an infinite set we went to a finite set
+    * We don't know yet how to implement this, so we will leave it alone for now
+    */
+  override def map[B](f: A => B): MySet[B] = ???
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = ???
+  override def foreach(f: A => Unit): Unit = ???
+
+  override def filter(predicate: A => Boolean): MySet[A] = ??? // Property based set
+
+  override def -(elem: A): MySet[A] = ???
+
+  override def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+
+  override def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+  override def unary_! : MySet[A] = new EmptySet[A]
+}
+
+/**
+  * Property based set is a set of all elements of type A which satisfy a property
+  * In math terms:
+  * { x in A | property(x) }
+  */
+class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
+  override def contains(element: A): Boolean = property(element) // the set contains this item if the property holds
+
+  /**
+    * What do we get when we add an element to a property based set
+    * { x in A | property(x) } + element = { x in A | property(x) || x == element }
+    */
+  override def +(element: A): MySet[A] = new PropertyBasedSet[A](x => property(x) || x == element)
+
+  /**
+    * What do we get when we add an element to a property based set
+    * { x in A | property(x) } ++ anotherSet = { x in A | property(x) || anotherSet contains x }
+    */
+  override def ++(anotherSet: MySet[A]): MySet[A] = new PropertyBasedSet[A](x => property(x) || anotherSet(x))
+
+  /**
+    * We are avoiding for now
+    */
+  override def map[B](f: A => B): MySet[B] = politerlyFail
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = politerlyFail
+  override def foreach(f: A => Unit): Unit = politerlyFail
+
+  /**
+    * A set where the property and the predicate hold for x
+    */
+  override def filter(predicate: A => Boolean): MySet[A] = new PropertyBasedSet[A](x => property(x) && predicate(x))
+
+  override def -(elem: A): MySet[A] = filter(_ != elem)
+
+  override def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+
+  override def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](x => !property(x)) // we are flipping the predicate (property) here
+
+  /**
+    * This is the case for map and flatMap, you don't know quite what will come out on the other side
+    * If you man an infinite set, you will get a set, but you will not know if that set is finite or not, which breaks the whole point of a set
+    */
+  def politerlyFail = throw new IllegalArgumentException("Really deep rabbit hole!") // for flat and
+}
+
 
 class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
   override def contains(element: A): Boolean = element == head || tail.contains(element)
@@ -66,6 +162,46 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
   override def foreach(f: A => Unit): Unit = {
     f(head) // apply f to head
     tail foreach f // call f on the tail recursively
+  }
+  override def -(elem: A): MySet[A] =
+    if (head == elem) tail
+    else tail - elem + head // we will run a recursive operation here on the tail each time, while putting head back
+
+  override def --(anotherSet: MySet[A]): MySet[A] = this filter !anotherSet
+
+  override def &(anotherSet: MySet[A]): MySet[A] = this filter anotherSet // intersection and filter is the same thing
+
+  // new operator
+  override def unary_! : MySet[A] = {
+    new MySet[A] {
+      override def contains(element: A): Boolean = ???
+
+      override def +(element: A): MySet[A] = ???
+
+      override def ++(anotherSet: MySet[A]): MySet[A] = ???
+
+      override def map[B](f: A => B): MySet[B] = ???
+
+      override def flatMap[B](f: A => MySet[B]): MySet[B] = ???
+
+      override def filter(predicate: A => Boolean): MySet[A] = ???
+
+      override def foreach(f: A => Unit): Unit = ???
+
+      /**
+        * Exercise:
+        * - removing an element
+        * - intersection with another set
+        * - difference with another set
+        */
+      override def -(elem: A): MySet[A] = ???
+
+      override def --(anotherSet: MySet[A]): MySet[A] = ???
+
+      override def &(anotherSet: MySet[A]): MySet[A] = ???
+
+      override def unary_! : MySet[A] = ???
+    }
   }
 }
 
--- src/playground/ScalaPlayground.scala
+++ src/playground/ScalaPlayground.scala
@@ -24,3 +24,31 @@ object ScalaPlayground  extends App {
   val solution = Solution.multiply("123", "456")
 
 }
+
+object Solution {
+  def exist(board: Array[Array[Char]], word: String): Boolean = {
+    for {
+      i <- 0 to board.length
+      j <- 0 to board(i).length
+    } yield {
+      def recurse(i: Int, j: Int, board: Array[Array[Char]], word: String, index: Int): Option[Boolean] = {
+        if (board(i)(j) == word(index)) {
+          if (index == word.length) return Option(true)
+          val temp = board(j)(j)
+          board(i)(j) = '.'
+          val newIndex = index + 1
+          val result = {
+              recurse(i + 1, j, board, word, newIndex)
+                .orElse(recurse(i - 1, j, board, word, newIndex)
+                  .orElse(recurse(i, j + 1, board, word, newIndex)
+                    .orElse(recurse(i, j - 1, board, word, newIndex).orElse(Option(false))
+          }
+          board(i)(j) = temp
+          result
+        }
+        else Option(false)
+      }
+      recurse(i, j, board, word, 0)
+    }
+  }
+}
